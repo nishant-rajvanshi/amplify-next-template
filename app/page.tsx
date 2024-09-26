947e4555c1e357984e09ddecfca4872ecb184a4c
Nishant Rajvanshi <nishant@computerware.in>
2024-09-26 16:23:54 +0530

    added authentication

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
+import { Authenticator } from "@aws-amplify/ui-react";
 
 Amplify.configure(outputs);
 
@@ -31,13 +32,20 @@ export default function App() {
     });
   }
 
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id });
+  }
+
   return (
+    <Authenticator>
+      {({signOut, user})=>(
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li onClick={() => deleteTodo(todo.id)} 
+          key={todo.id}>{todo.content}</li>
         ))}
       </ul>
       <div>
@@ -47,6 +55,9 @@ export default function App() {
           Review next steps of this tutorial.
         </a>
       </div>
+      <button onClick={signOut}>Sign out</button>
     </main>
+    )}
+    </Authenticator>
   );
 }
